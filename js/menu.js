b3f21b3653858099b6aaf51c3c6e07468ac064f9
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2024-10-09 11:40:34 -0300

    Script do menu usando JS

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+// Selecionando o link com a palavra Menu
+const botaoMenu = document.querySelector(".titulo-menu a");
+
+// Selecionando a lista de links do menu
+const listaDeLinks = document.querySelector(".links-menu");
+
+// Ouvinte de Evento para o click no botão menu
+botaoMenu.addEventListener("click", function( event ){
+    /* Anulando o comportamento padrão de recarregar a 
+    página através de um link */
+    event.preventDefault();
+
+    /* Abrimos/Fechamos a lista de links usando o toggle na classe aberto */
+    listaDeLinks.classList.toggle("aberto");
+
+    if(listaDeLinks.classList.contains("aberto")){
+        botaoMenu.innerHTML = "Fechar &times;";
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;";
+    }
+});
+
+
+
