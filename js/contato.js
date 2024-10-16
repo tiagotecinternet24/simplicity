175d2f2212de23b687b25d85790622cc690238aa
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2024-10-16 11:31:20 -0300

    Uso da API Formspree para formulário

--- js/contato.js
+++ js/contato.js
@@ -77,3 +77,36 @@ botaoBuscar.addEventListener("click", async function(){
     }    
 
 }); // final do evento do botão
+
+
+/* Código do Formspree */
+var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Obrigado! Mensagem enviada com sucesso. Aguarde nosso retorno.";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! Algo de errado não está certo... tente novamente mais tarde."
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! Houve um erro... fale com o administrador pelo email <a href='mailto:admin@simplicity.com.br'>admin@simplicity.com.br</a>"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
